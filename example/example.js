616e630ba3ae02ecd64db6098a10d06f491bbb3b
yankechao <jzykc@hotmail.com>
2020-08-28 18:19:55 +0800

    demo style

--- example/example.js
+++ example/example.js
@@ -1,6 +1,15 @@
 import * as prstShape from 'prst-shape-transform'
 const shapeNames = Object.keys(prstShape)
+
+const svgs = []
 for(let shape of shapeNames) {
-    const path = prstShape[shape](100, 100)
-    console.log(path.length, path)
+    let svg = `<div style="text-align: center; margin: 15px; padding: 5px;display:inline-block"><svg width=100 height=100>`
+    const paths = prstShape[shape](100, 100)
+    paths.forEach(path => {
+        svg += `<path d=${path}></path>`
+    })
+    svg += `</svg><p style="font-size: 12px">${shape}</p></div>`
+    svgs.push(svg)
 }
+console.log(svgs[0])
+document.documentElement.innerHTML = svgs.slice(0).join('')
